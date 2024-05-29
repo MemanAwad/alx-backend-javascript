c6c9a21f9828f67d55cca2a9e35bea85e3d0c45d
MemanAwad <meman1awad@gmail.com>
2024-05-30 01:21:31 +0300

    use all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise
+    .all([uploadPhoto(), createUser()])
     .then((result) => {
       const [photoResult, userResult] = result;
       console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
