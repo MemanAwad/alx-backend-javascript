539340164f85873feb73db74d99d1666be540db8
MemanAwad <meman1awad@gmail.com>
2024-05-30 01:16:12 +0300

    promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,10 @@
 export default function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({status: 200, body: 'success'});
+      resolve({
+        status: 200,
+        body: 'success',
+      });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
