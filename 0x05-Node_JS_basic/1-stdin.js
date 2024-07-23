2507e5239e49a0dc33d1dc7031a572dfc2dce617
MemanAwad <meman1awad@gmail.com>
2024-07-24 01:37:45 +0300

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,26 +1,19 @@
-// Importing the readline module to handle input/output
+/* function to read from stdin */
 const readline = require('readline');
 
-// Creating an interface to read lines from stdin and output to stdout
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// Displaying the initial message
-console.log('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
-// Waiting for the user input
 rl.question('', (name) => {
-  // Displaying the user's input
   console.log(`Your name is: ${name}`);
-
-  // Closing the readline interface and displaying the closing message
   rl.close();
 });
 
-// Listening for the close event to display the closing message
 rl.on('close', () => {
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
   process.exit(0);
 });
