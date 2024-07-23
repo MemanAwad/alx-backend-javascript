54c505602051e513fb46ee884905f61dc46ae89c
MemanAwad <meman1awad@gmail.com>
2024-07-24 01:11:34 +0300

    fix eslint

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
-/* this is a function that print to the stdout*/
+/* this is a function that print to the stdout */
 
 const displayMessage = (msg) => {
-    console.log(msg)
-}
+  console.log(msg);
+};
 
 module.exports = displayMessage;
