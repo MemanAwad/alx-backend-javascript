d6bb5c0f17cec7a54f5f862efff936f1b3257a25
MemanAwad <meman1awad@gmail.com>
2024-07-28 13:01:16 +0300

    fix

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,4 +1,5 @@
 export default class HolbertonCourse {
+
   constructor(name, length, students) {
     this.name = name;
     this.length = length;
@@ -9,30 +10,44 @@ export default class HolbertonCourse {
     return this._name;
   }
 
-  set name(vname) {
-    if (typeof vname !== 'string') {
+  
+  set name(value) {
+    if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this._name = vname;
-  } 
+    this._name = value;
+  }
 
+  
   get length() {
     return this._length;
   }
 
-  set lenght(vlenght) {
-    if (typeof vlenght !== 'number') {
+  
+  set length(value) {
+    if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this._lenght = vlenght;
+    this._length = value;
   }
 
+  /**
+   * Gets the names of students in this course.
+   */
   get students() {
     return this._students;
   }
 
-  set students(vstudents) {
-    this._students = vstudents;
+  /**
+   * Sets the names of students in this course.
+   */
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
   }
-
 }
